1246af56e07fbff2859bd4c2f2c6f6cf88bbb0c0
Vault CMS User <vault-cms@example.com>
2026-05-20 03:51:03 -0400

    Initial commit from Vault CMS

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  vite: {
+    assetsInclude: ['**/*.base', '**/.obsidian/**', '**/_bases/**'],
+    server: {
+      watch: {
+        ignored: ['**/.obsidian/**', '**/_bases/**', '**/bases/**', '**/_home/**', '**/home/**', '**/_base/**', '**/base/**']
+      }
+    }
+  },
   output: "static",
   prefetch: true,
   compressHTML: true,
